78138f0c14ae5a89dc9db8d563b69540889c9554
hzmarrou <zmarrouhoussam@email.com>
2026-04-28 20:57:54 +0100

    returning specific part of a string or array

---
+++ last-first-kiss.js
@@ -0,0 +1,6 @@
+const first = (f) => f[0]
+const last = (l) => l[l.length-1]
+const kiss = (s) => [first(s), last(s)]
+// console.log(first('hello'))
+// console.log(last('hello'))
+// console.log(kiss('hello'))
