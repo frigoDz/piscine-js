acd2f630ccb79e3cc257fdccacdce16bb304b962
hzmarrou <zmarrouhoussam@email.com>
2026-04-28 23:15:29 +0100

    I multiply rather than divising

--- dog-years.js
+++ dog-years.js
@@ -6,27 +6,27 @@ function dogYears(planet, ageInSec) {
     age = earthAge
     break
     case "mercury":
-    age = earthAge * .2408467
+    age = earthAge / .2408467
       break
     case "venus":
-    age = earthAge * .61519726
+    age = earthAge / .61519726
       break
     case "mars":
-    age = earthAge * 1.8808158
+    age = earthAge / 1.8808158
       break
     case "jupiter":
-    age = earthAge * 11.862615
+    age = earthAge / 11.862615
       break
     case "saturn":
-    age = earthAge * 29.447498
+    age = earthAge / 29.447498
       break
     case "uranus":
-    age = earthAge * 84.016846
+    age = earthAge / 84.016846
       break
     case "neptune":
-    age = earthAge * 164.79132
+    age = earthAge / 164.79132
       break
   }
   return Number((age * 7).toFixed(2))
 }
-// console.log(dogYears("earth", 1000000000))
+// console.log(dogYears("mercury", 2134835688))
