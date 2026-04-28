f874ab93e0519e53d7fa8a1f0e8d21730e32b03b
hzmarrou <zmarrouhoussam@email.com>
2026-04-28 23:08:22 +0100

    exercise 10: converting the dog seconds to years then match them with the planet and return the year * 7

---
+++ dog-years.js
@@ -0,0 +1,32 @@
+function dogYears(planet, ageInSec) {
+  const earthAge = ageInSec/60/60/24/365.25
+  let age = 0
+  switch (planet) {
+    case "earth":
+    age = earthAge
+    break
+    case "mercury":
+    age = earthAge * .2408467
+      break
+    case "venus":
+    age = earthAge * .61519726
+      break
+    case "mars":
+    age = earthAge * 1.8808158
+      break
+    case "jupiter":
+    age = earthAge * 11.862615
+      break
+    case "saturn":
+    age = earthAge * 29.447498
+      break
+    case "uranus":
+    age = earthAge * 84.016846
+      break
+    case "neptune":
+    age = earthAge * 164.79132
+      break
+  }
+  return Number((age * 7).toFixed(2))
+}
+// console.log(dogYears("earth", 1000000000))
