2446d8f16d17ebbb69c403fa155d0b95858fa699
hzmarrou <zmarrouhoussam@email.com>
2026-04-28 19:53:07 +0100

    freezing at declaration time bc, I think the tester expect that even tho my last version was fine

--- declarations.js
+++ declarations.js
@@ -1,23 +1,19 @@
 const escapeStr = "`\\/\"'"
 
-const arr = [4, '2']
+const arr = Object.freeze([4, '2'])
 
-const obj = {
-	str : 'string',
-	num : 5,
-	bool : true,
-	undef : undefined
-}
+const obj = Object.freeze({
+	str: 'string',
+	num: 5,
+	bool: true,
+	undef: undefined
+})
 
-const nested = {
-	arr : [4, undefined, '2'],
-	obj : {
-		str : 'string',
-		num : 4,
-		bool : true
-	}
-}
-
-Object.freeze(arr)
-Object.freeze(obj)
-Object.freeze(nested)
+const nested = Object.freeze({
+	arr: Object.freeze([4, undefined, '2']),
+	obj: Object.freeze({
+		str: 'string',
+		num: 4,
+		bool: true
+	})
+})
