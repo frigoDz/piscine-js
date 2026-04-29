61ac4fbf0551de18c5eb88fde12addb0eba67e3b
hzmarrou <zmarrouhoussam@email.com>
2026-04-29 21:08:19 +0100

    handling the negative numbers in mulitply and fixing the while in the divide and modulo

--- elementary.js
+++ elementary.js
@@ -1,25 +1,38 @@
 function multiply(a, b) {
   let res = 0
+  let sign = false
+  if (a < 0) {
+    sign = true
+    a = -a 
+  }
+  if (b < 0 && !sign) {
+    sign = true
+    b = -b
+
+  }
   while(a > 0) {
     res += b
     a--
+  }
+  if (sign === true) {
+    res = -res
   }
   return res
 }
 function divide(a, b) {
   let res = 0 
-  while(a > 0 && a > b) {
+  while(a > 0 && a >= b) {
     a = a - b 
     res++
   }
   return res
 }
 function modulo(a, b) {
-  while(a > 0 && a > b) {
+  while(a > 0 && a >= b) {
     a = a - b 
   }
   return a
 }
-// console.log(multiply(3, 7))
-// console.log(divide(10, 3))
-// console.log(modulo(10, 3))
+// console.log(multiply(-3, -7))
+// console.log(divide(10, 2))
+// console.log(modulo(10, 2))
