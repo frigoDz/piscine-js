38f0655e5efd421a685007220fe37d7128f50e9a
hzmarrou <zmarrouhoussam@email.com>
2026-04-29 01:05:04 +0100

    two function firs one checking if the number positive and seconde one give the abslute of the arg num

---
+++ abs.js
@@ -0,0 +1,15 @@
+function isPositive(num){
+  if (num < 0) {
+    return false
+  }
+  return true
+}
+function abs(num){
+  if (num < 0) {
+    return num = -num
+  }
+  return num
+}
+
+// console.log(isPositive(-23232))
+// console.log(abs(-12))
