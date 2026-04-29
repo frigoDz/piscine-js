70603defaaf455ac292434d3fa92904a8e2ed2cb
hzmarrou <zmarrouhoussam@email.com>
2026-04-29 01:18:00 +0100

    return the small or the big number in the param

---
+++ min-max.js
@@ -0,0 +1,18 @@
+function max(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+function min(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+// console.log(max(10, 5));
+// console.log(min(3, 8));
