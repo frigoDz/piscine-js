e5e937e89fe570e1d473fa57b22f6e52a4d8fb6a
hzmarrou <zmarrouhoussam@email.com>
2026-04-29 21:30:02 +0100

    handling negative in divition and returning always positive in modilo

--- elementary.js
+++ elementary.js
@@ -21,18 +21,33 @@ function multiply(a, b) {
 }
 function divide(a, b) {
   let res = 0 
-  while(a > 0 && a >= b) {
+  let sign = false
+  if (a < 0) {
+    sign = true
+    a = -a 
+  }
+  if (b < 0 && !sign) {
+    sign = true
+    b = -b
+  }
+  while(a >= b) {
     a = a - b 
     res++
   }
+  if (sign === true) {
+    res = -res
+  }
   return res
 }
+
 function modulo(a, b) {
+  if (a < 0) a = -a 
+  if (b < 0) b = -b
   while(a > 0 && a >= b) {
     a = a - b 
   }
   return a
 }
-// console.log(multiply(-3, -7))
-// console.log(divide(10, 2))
+// console.log(multiply(-3, 7))
+// console.log(divide(-10, -2))
 // console.log(modulo(10, 2))
