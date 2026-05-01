072158c0766646e814ef08bca318812ce2d24ed3
hzmarrou <zmarrouhoussam@email.com>
2026-05-02 00:56:44 +0100

    fixing the keepFirstLast range of overflow

--- keep-cut.js
+++ keep-cut.js
@@ -29,12 +29,12 @@ function keepLast(str) {
   return str.slice(-2)
 }
 function keepFirstLast(str) {
-  if(str.length <= 2) return str
+  if(str.length <= 3) return str
   return  str.slice(0,2) + str.slice(-2)
 }
 // console.log(cutFirst("h"))
 // console.log(cutLast("h"))
 // console.log(cutFirstLast("hdd"))
-// console.log(keepFirst("hello"))
+// console.log(keepFirst("fjdfk"))
 // console.log(keepLast("hello"))
-// console.log(keepFirstLast("kk"))
+// console.log(keepFirstLast("afldd"))
