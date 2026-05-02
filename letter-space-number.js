a0988edbdd437e9cecd239d97ffc8ab5750746bb
hzmarrou <zmarrouhoussam@email.com>
2026-05-02 13:55:32 +0100

    small fix: adding feedback to match

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,6 +1,6 @@
 function letterSpaceNumber(str) {
-  const regex = /[a-zA-Z]\s[0-9](?=\W)/g
-  return str.match(regex)
+  const regex = /[a-zA-Z]\s[0-9](?!\d)(?![a-zA-Z])/g
+  return str.match(regex) || []
 }
-// const string = "example 1, example 20"
+// const string = "I like 7up."
 // console.log(letterSpaceNumber(string))
