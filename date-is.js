00ebd228989fc685534e286dde709554faa6ad5c
hzmarrou <zmarrouhoussam@email.com>
2026-05-06 23:48:08 +0100

    fixing isValid, and adding missing const

--- date-is.js
+++ date-is.js
@@ -1,4 +1,7 @@
 function isValid(date) {
+  if (typeof date == 'number') {
+    return !isNaN(date)
+  }
   return date && date.getTime && !isNaN(date.getTime())
 }
 function isAfter(date1, date2) {
@@ -8,10 +11,10 @@ function isBefore(date1, date2) {
   return date2 > date1
 }
 function isFuture(date) {
-  newdate = new Date()
+  const newdate = new Date()
   return isValid(date) && newdate < date 
 }
 function isPast(date)  {
-  newdate = new Date()
+  const newdate = new Date()
   return isValid(date) && newdate > date
 }
