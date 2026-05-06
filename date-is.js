b89b4fd0ea68f0323450b169e37e38c61a2033c2
hzmarrou <zmarrouhoussam@email.com>
2026-05-06 23:38:41 +0100

    checking dates

---
+++ date-is.js
@@ -0,0 +1,17 @@
+function isValid(date) {
+  return date instanceof Date && !isNaN(date.getTime())
+}
+function isAfter(date1, date2) {
+  return date1 > date2
+}
+function isBefore(date1, date2) {
+  return date2 > date1
+}
+function isFuture(date) {
+  newdate = new Date()
+  return isValid(date) && newdate < date 
+}
+function isPast(date)  {
+  newdate = new Date()
+  return isValid(date) && newdate > date
+}
