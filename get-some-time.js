635d7960df32e5c9dbeaf706fe49c6fc98bac2b8
hzmarrou <zmarrouhoussam@email.com>
2026-05-07 03:02:16 +0100

    get the monday of the expected week in the year given

---
+++ get-some-time.js
@@ -0,0 +1,26 @@
+function firstDayWeek(num, str) {
+  const day1 = new Date(str+"-01-01")
+  let y = 0
+  if (day1.getDay() != 1) {
+    if(num == 1) {
+      return "01-01-" + str
+    }
+    let i = day1.getDay()
+    if (i == 0) {
+      i = i+7
+    }
+    while (i != 1) {
+      i--
+      y++
+    }
+  }
+  day1.setDate(day1.getDate() - y)
+  const weekDay = (num -1) * 7
+  day1.setDate(day1.getDate() + weekDay)
+  const day = String(day1.getDate()).padStart(2, "0")
+  const month = String(day1.getMonth() + 1).padStart(2, "0")
+  const year = day1.getFullYear()
+
+  return day + "-" + month + "-" + year
+}
+// console.log(firstDayWeek(2,"2025"))
