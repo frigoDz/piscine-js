33e3d576319bebf70e5b7735655e5e1451c1b41a
hzmarrou <zmarrouhoussam@email.com>
2026-05-08 00:54:17 +0100

    addding initial value in the other func

--- using-reduce.js
+++ using-reduce.js
@@ -1,16 +1,16 @@
-function  adder(arr) {
-  return arr.reduce((acu,cur) => acu + cur)
+function  adder(arr, initialValue = 0) {
+  return arr.reduce((acu,cur) => acu + cur,initialValue)
 }
 // const arr = [2,4,3,2]
 // console.log(adder(arr))
-function sumOrMul(arr) {
+function sumOrMul(arr, initialValue = 0) {
   return arr.reduce((acu,cur) => {
     if (acu % 2 == 0) {
       return acu * cur
     } else {
       return acu + cur
     }
-  })
+  }, initialValue)
 }
 // console.log(sumOrMul(arr))
 function funcExec(arrfunc, initialValue = 0) {
