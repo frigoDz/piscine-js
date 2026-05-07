9425513463343340d7480dd5e79c2dfb13c46b0a
hzmarrou <zmarrouhoussam@email.com>
2026-05-07 22:58:58 +0100

    writing some fonction to help me understand how filter work

---
+++ using-filter.js
@@ -0,0 +1,73 @@
+function filterShortStateName(arr) {
+  const res = arr.filter((word) => word.length < 7)
+  return res
+}
+// const arr = ["123", "1234", "12345", "123456", "12434567", "123456789"]
+// console.log(filterShortStateName(arr))
+function filterStartVowel(arr) {
+  const res = arr.filter((word) => 'aeiouAEIOU'.includes(word[0]))
+  return res
+}
+// const arr = ["ali", "rebo", "Elkjfd", "mia"]
+// console.log(filterStartVowel(arr))
+function filter5Vowels(arr) {
+  const res = arr.filter((word) => {
+    let count = 0
+    for (let i = 0; i < word.length; i++) {
+      if ('aeiouAEIOU'.includes(word[i])) {
+        count++
+      }
+    }
+    if(count >= 5) {
+      return word
+    }
+  })
+  return res
+}
+// const arr = ["aeiei", "rebo", "Elkjfd", "maaaaaaia"]
+// console.log(filter5Vowels(arr))
+function filter1DistinctVowel(arr) {
+  const res = arr.filter((word) => {
+    let tmp = []
+    for (let i = 0; i < word.length; i++) {
+      if ('aeiouAEIOU'.includes(word[i])) {
+        tmp.push(word[i])
+      }
+    }
+    check = new Set(tmp)
+    if(tmp.length !== 0 && check.size == 1) {
+      return word
+    }
+  })
+  return res
+
+}
+// const arr = ["aeiei", "rebeeeeeeeeeeeeeeeee", "Elkjfd", "maaaaaaia"]
+// console.log(filter1DistinctVowel(arr))
+function multiFilter(arr) {
+  return arr.filter((obj) => {
+    let count = 0
+    for(let i = 0; i < obj.tag.length; i++) {
+      if ('aeiouAEIOU'.includes(obj.tag[i])){
+        count++
+      }
+    }
+    return (obj.capital.length >= 8 && !('aeiouAEIOU'.includes(obj.name[0])) && obj.region !== "South" && count !== 0)
+  })
+}
+
+// const arrbj = [
+//   {
+//     capital: "CasablancaCity",
+//     name: "John",
+//     tag: "alpha",
+//     region: "North"
+//   },
+//   {
+//     capital: "NYC",
+//     name: "elice",
+//     tag: "bcdfg",
+//     region: "South"
+//   },
+// ]
+// console.log(multiFilter(arrbj))
