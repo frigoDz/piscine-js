c86143720aa2e894f9fe76e4e474ad3c04a5f6dd
hzmarrou <zmarrouhoussam@email.com>
2026-05-08 00:49:16 +0100

    learning how to use reduce

---
+++ using-reduce.js
@@ -0,0 +1,23 @@
+function  adder(arr) {
+  return arr.reduce((acu,cur) => acu + cur)
+}
+// const arr = [2,4,3,2]
+// console.log(adder(arr))
+function sumOrMul(arr) {
+  return arr.reduce((acu,cur) => {
+    if (acu % 2 == 0) {
+      return acu * cur
+    } else {
+      return acu + cur
+    }
+  })
+}
+// console.log(sumOrMul(arr))
+function funcExec(arrfunc, initialValue = 0) {
+  return arrfunc.reduce((acu,curf) => curf(acu),initialValue )
+}
+// const funcs = [
+//   x => x * 10000,
+//   x => x * 500
+// ]
+// console.log(funcExec(funcs, 1))
