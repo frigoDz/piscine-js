be2f6f38bfbe4290db844071d53ac5551f44df59
hzmarrou <zmarrouhoussam@email.com>
2026-05-08 02:08:28 +0100

    it's done

--- for-each.js
+++ for-each.js
@@ -1,9 +1,11 @@
-function forEach(arr, func) {
-  let res = ""
+function forEach(arr, func) { 
   for(let i = 0; i < arr.length; i++ {
     func(arr[i], i, arr)
   }
-  return undefined
 }
 // const arr = ["a", 'b','v']
 // forEach(arr, console.log)
+//
+//
+// x[1, 2, 3, 4]
+// x[6, 7, 8, 9]
