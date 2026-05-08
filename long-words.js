2610b11c2e490c497dc6cd6bd55d8ef2494b33bd
hzmarrou <zmarrouhoussam@email.com>
2026-05-08 01:20:18 +0100

    exercice 4 long-word checking a word with using some nd every

---
+++ long-words.js
@@ -0,0 +1,9 @@
+function longWords(arr) {
+  return arr.every((word) typeof(word) == 'string' && word.length >= 5)
+}
+function oneLongWord(arr) {
+  return arr.some((word) typeof(word) == 'string' && word.length >= 10)
+}
+function noLongWords(arr) {
+  return arr.every((word) typeof(word) != 'string' && word.length < 7)
+}
