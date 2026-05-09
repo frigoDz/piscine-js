c070f60b8c8c6c0c42586068bc13568097ca81a0
hzmarrou <hzmarrou@gmail.com>
2026-05-09 23:28:50 +0100

    finishing build-brick-and-break

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,1 +1,29 @@
-
+export const build = (n) => {
+  let i = 1
+  const interval = setInterval(() => {
+    if (i > n) {
+      clearInterval(interval)
+      return
+    }
+    const brick = document.createElement("div")
+    brick.id = "brick-" + String(i)
+    if (i % 3 == 2) {
+      brick.dataset.foundation = true
+    }
+    document.body.append(brick)
+    i++
+  }, 100)
+}
+export const repair = (...ids) => {
+  ids.forEach((id) => {
+    let div = document.getElementById(id)
+    if (!div) return
+    if (div.hasAttribute("data-foundation")) div.dataset.repaired = "in progress"
+    else div.dataset.repaired = "true"
+  })
+}
+export const destroy = () => {
+  const bricks = document.querySelectorAll("div")
+  const lastBrick = bricks[bricks.length - 1]
+  if (lastBrick) lastBrick.remove()
+}
