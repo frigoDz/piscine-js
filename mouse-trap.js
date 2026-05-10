0a82796a46da186f0aff383e8a996be2a9e45f4e
hzmarrou <hzmarrou@gmail.com>
2026-05-10 06:54:18 +0100

    a very fun project of a circle tracing mouse position and getting trap if goes inside the box

---
+++ mouse-trap.js
@@ -0,0 +1,38 @@
+export const createCircle = () => {
+  document.addEventListener("click", mou => {
+    let div = document.createElement("div")
+    div.classList.add("circle")
+    div.style.background = "white"
+    div.style.position = "absolute"
+    div.style.left = mou.clientX + "px"
+    div.style.top = mou.clientY + "px"
+    document.body.append(div)
+  })
+}
+export const moveCircle = () => {
+  addEventListener("mousemove", (event) => {
+    let all = document.querySelectorAll(".circle")
+    let lastOne = all[all.length -1]
+    if (!lastOne) return
+    let box = document.querySelector(".box") 
+    let boxRect = box.getBoundingClientRect()
+    let circleRect = lastOne.getBoundingClientRect()
+    let inside =
+      circleRect.left > boxRect.left &&
+      circleRect.right < boxRect.right &&
+      circleRect.top > boxRect.top &&
+      circleRect.bottom < boxRect.bottom
+    if (inside) {
+      lastOne.style.background = "var(--purple)"
+      return
+    }
+    lastOne.style.left = event.clientX + "px"
+    lastOne.style.top = event.clientY + "px"
+  })
+}
+
+export const setBox = () => {
+  let div =document.createElement("div")
+  div.classList.add("box")
+  document.body.append(div)
+}
