57110e77da065ab5472f9331f6a6b1b22499313c
hzmarrou <hzmarrou@gmail.com>
2026-05-14 00:58:13 +0100

    recreating the functions

--- manipulate-values.js
+++ manipulate-values.js
@@ -1,23 +1,28 @@
-function filterValues(nutrients) {
-  const entries = Object.entries(nutrients)
-  const res = entries.filter((entry) => entry[1] <= 12)
-  // console.log(Object.prototype.toString.call(res).slice(8,-1))
-  return Object.fromEntries(res)
+function filterValues(obj, func) {
+  const res = {}
+  for(let key of Object.keys(obj)) {
+    let v = obj[key]
+    if(func(v)) res[key] = v
+  }
+  return res
 }
-// console.log(filterValues({ carbohydrates: 12, protein: 20, fat: 5 }))
-function mapValues(nutrients) {
-  const entries = Object.entries(nutrients)
-  const res = entries.map((entry) => {
-    entry[1] = entry[1] + 1 
-    return entry
-  })
-  // console.log(Object.prototype.toString.call(res).slice(8,-1))
-  return Object.fromEntries(res)
+function mapValues(obj, func) {
+  const res = {}
+  for(let key of Object.keys(obj)) res[key] = func(obj[key])
+  return res
 }
-// console.log(mapValues({ carbohydrates: 12, protein: 20, fat: 5 }))
-function reduceValues(nutrients) {
-  const val = Object.values(nutrients)
-  const res = val.reduce((acc ,cur) => acc + cur)
+function reduceValues(obj, func, init = 0) {
+  let res = init
+  for(let key of Object.keys(obj)) res = func(res, obj[key])
   return res
 } 
-// console.log(reduceValues({ carbohydrates: 12, protein: 20, fat: 5 }))
+const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
+
+console.log(filterValues(nutrients, (nutrient) => nutrient <= 12))
+// output: { carbohydrates: 12, fat: 5 }
+
+console.log(mapValues(nutrients, (v) => v+1))
+// output: { carbohydrates: 13, protein: 21, fat: 6 }
+
+console.log(reduceValues(nutrients, (acc, cr) => acc + cr))
+// output: 37
