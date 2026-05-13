b43d914a3f7982a4a12866d3fc95a384aed8299b
hzmarrou <hzmarrou@gmail.com>
2026-05-14 00:39:20 +0100

    defining entries and val

--- manipulate-values.js
+++ manipulate-values.js
@@ -1,12 +1,12 @@
 function filterValues(nutrients) {
-  entries = Object.entries(nutrients)
+  const entries = Object.entries(nutrients)
   const res = entries.filter((entry) => entry[1] <= 12)
   // console.log(Object.prototype.toString.call(res).slice(8,-1))
   return Object.fromEntries(res)
 }
 // console.log(filterValues({ carbohydrates: 12, protein: 20, fat: 5 }))
 function mapValues(nutrients) {
-  entries = Object.entries(nutrients)
+  const entries = Object.entries(nutrients)
   const res = entries.map((entry) => {
     entry[1] = entry[1] + 1 
     return entry
@@ -16,7 +16,7 @@ function mapValues(nutrients) {
 }
 // console.log(mapValues({ carbohydrates: 12, protein: 20, fat: 5 }))
 function reduceValues(nutrients) {
-  val = Object.values(nutrients)
+  const val = Object.values(nutrients)
   const res = val.reduce((acc ,cur) => acc + cur)
   return res
 } 
