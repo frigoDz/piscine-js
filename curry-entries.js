646b2205a6991b52c76118900c6b54d26363c8fc
hzmarrou <hzmarrou@gmail.com>
2026-05-14 07:10:20 +0100

    forgot the object defined and not used

--- curry-entries.js
+++ curry-entries.js
@@ -42,13 +42,13 @@ function mapAverage(obj) {
     return [name, { averageScore: averageScore }]
   })(obj)
 }
-const personnel = {
-  lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
-  sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
-  zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
-  ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
-  calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
-}
+// const personnel = {
+//   lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
+//   sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+//   zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+//   ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
+//   calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
+// }
 // console.log(filterForce(personnel))
 // console.log(reduceScore(personnel))
 // console.log(mapAverage(personnel))
