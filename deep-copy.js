c58703601ac6b87f4ce536d5ee3eff07a0b8a5f4
hzmarrou <zmarrouhoussam@email.com>
2026-05-15 17:54:59 +0100

    I got the error now

--- deep-copy.js
+++ deep-copy.js
@@ -1,10 +1,9 @@
 function deepCopy(obj) {
   const res = {}
   const resarr = []
-  /*if (obj instanceof RegExp) {
+  if (obj instanceof RegExp) {
     return new RegExp(obj)
-  }else */
-  if (!Array.isArray(obj)){
+  }else if (!Array.isArray(obj)){
     for (const [key,value] of Object.entries(obj)) {
       res[key] = value 
       if (typeof obj[key] == 'object') deepCopy(obj[key])
@@ -14,8 +13,7 @@ function deepCopy(obj) {
     for(let i = 0; i < obj.length;i++) {
       //if (!res[i]) res[i] = {}
       //if (Array.isArray(res[i])) res[i] = deepCopy(res[i]) 
-      resarr[i] = obj[i]
-      if (Array.isArray(obj[i])) deepCopy(obj[i])
+      resarr.push(deepCopy(obj[i]))
     }
     return resarr
     /*Object.entries(obj).forEach(([key,value]) => {
@@ -26,7 +24,7 @@ function deepCopy(obj) {
     return res = obj
   }
 }
-/*const arr = [1, 'a']
+const arr = [1, 'a']
 const copy = deepCopy(arr)
 console.log(copy)
 console.log(arr)
@@ -35,4 +33,4 @@ const coppy = deepCopy(obj)
 
 console.log(coppy)
 console.log(coppy)
-console.log(obj)*/
+console.log(obj)
