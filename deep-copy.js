6de631bcc1277414394dd23742ec9ea88ce5c84e
hzmarrou <you@example.com>
2026-05-15 16:34:48 +0100

    ewa khdem

--- deep-copy.js
+++ deep-copy.js
@@ -1,5 +1,6 @@
 function deepCopy(obj) {
   const res = {}
+  const resarr = []
   /*if (obj instanceof RegExp) {
     return new RegExp(obj)
   }else */
@@ -13,10 +14,10 @@ function deepCopy(obj) {
     for(let i = 0; i < obj.length;i++) {
       //if (!res[i]) res[i] = {}
       //if (Array.isArray(res[i])) res[i] = deepCopy(res[i]) 
-      res[i] = obj[i]
+      resarr[i] = obj[i]
       if (Array.isArray(obj[i])) deepCopy(obj[i])
     }
-    return res
+    return resarr
     /*Object.entries(obj).forEach(([key,value]) => {
       res[key] = deepCopy(value)
     })
@@ -25,7 +26,7 @@ function deepCopy(obj) {
     return res = obj
   }
 }
-/*const arr = ["hello", ["test","soft"]]
+/*const arr = [1, 'a']
 const copy = deepCopy(arr)
 console.log(copy)
 console.log(arr)
