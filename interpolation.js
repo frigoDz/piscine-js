6b6221e16a8d827a124ccb47ebaa5e04bd48b876
hzmarrou <zmarrouhoussam@email.com>
2026-05-17 05:53:37 +0100

    include start and excludes end

--- interpolation.js
+++ interpolation.js
@@ -2,13 +2,13 @@ function interpolation({ step, start, end, callback, duration }) {
   const gap = (end - start) / step
   const delay = duration / step
 
-  for (let i = 1; i <= step; i++) {
+  for (let i = 0; i < step; i++) {
     setTimeout(() => {
       callback([
         start + gap * i,
-        delay * i
+        delay * (i + 1)
       ])
-    }, delay * i)
+    }, delay * (i + 1))
   }
 }
 // interpolation({step: 5, start: 0, end: 1, duration: 10, callback: console.log})
