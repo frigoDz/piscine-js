77e9b7769aa92e56e584ce1e55df0a56b4f1da24
hzmarrou <zmarrouhoussam@email.com>
2026-05-17 07:10:28 +0100

    fix

--- is-winner.js
+++ is-winner.js
@@ -1,13 +1,17 @@
 async function isWinner(countryName) {
-  const country = await db.getWinner(countryName)
-  const wins = await db.getResults(country.id)
-  if (country.continent !== "Europe") {
-    return `${country.name} is not what we are looking for because of the continent`
+  try {
+    const country = await db.getWinner(countryName)
+    const wins = await db.getResults(country.id)
+    if (country.continent !== "Europe") {
+      return `${country.name} is not what we are looking for because of the continent`
+    }
+    if (wins.length < 3) {
+      return `${country.name} is not what we are looking for because of the number of times it was champion`
+    }
+    const years = wins.map(w => w.year).join(", ")
+    const results = wins.map(w => w.score).join(", ")
+    return `${country.name} won the FIFA World Cup in ${years} winning by ${results}`
+  } catch (err) {
+    return `${countryName} never was a winner`
   }
-  if (wins.length < 3) {
-    return `${country.name} is not what we are looking for because of the number of times it was champion`
-  }
-  const years = wins.map(w => w.year).join(", ")
-  const results = wins.map(w => w.score).join(", ")
-  return `${country.name} won the FIFA World Cup in ${years} winning by ${results}`
 }
