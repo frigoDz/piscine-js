7982ab8c720c4849a359e1d842484754ad245e31
hzmarrou <zmarrouhoussam@email.com>
2026-05-17 07:07:16 +0100

    add isWinner async function with FIFA world cup rules

---
+++ is-winner.js
@@ -0,0 +1,13 @@
+async function isWinner(countryName) {
+  const country = await db.getWinner(countryName)
+  const wins = await db.getResults(country.id)
+  if (country.continent !== "Europe") {
+    return `${country.name} is not what we are looking for because of the continent`
+  }
+  if (wins.length < 3) {
+    return `${country.name} is not what we are looking for because of the number of times it was champion`
+  }
+  const years = wins.map(w => w.year).join(", ")
+  const results = wins.map(w => w.score).join(", ")
+  return `${country.name} won the FIFA World Cup in ${years} winning by ${results}`
+}
