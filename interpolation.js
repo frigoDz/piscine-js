685bdd005c09fbb4ee0589bf162b713cd288164f
hzmarrou <zmarrouhoussam@email.com>
2026-05-17 05:40:01 +0100

    kfjds

---
+++ interpolation.js
@@ -0,0 +1,14 @@
+function interpolation({step, start, end, callback, duration}) {
+  const gap = (end - start) / step
+  const delay = duration / step
+
+  for (let i = 1; i < step; i++) {
+    setTimeout(() => {
+      callback([
+        start + gap * i,
+        delay * i
+      ])
+    }, delay * i)
+  }
+}
+// interpolation({step: 5, start: 0, end: 1, duration: 10, callback: console.log})
