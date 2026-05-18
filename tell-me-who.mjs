8612e1b4277a98af7f2d7e3258107201e56b79b4
hzmarrou <zmarrouhoussam@email.com>
2026-05-18 18:22:18 +0100

    let-me-who.mjs

---
+++ tell-me-who.mjs
@@ -0,0 +1,12 @@
+import { readdir } from "node:fs/promises"
+const dir = process.argv[2]
+const files = await readdir(dir)
+files.sort()
+const result = files.map((file, index) => {
+  const parts = file.split(".")
+  const [firstName, lastName] = parts[0].split("-")
+  const first = firstName.slice(0, 1).toUpperCase() + firstName.slice(1)
+  const last = lastName.slice(0, 1).toUpperCase() + lastName.slice(1)
+  return `${index + 1}. ${last} ${first}`
+})
+console.log(result.join("\n"))
