297c72f1f20bf7f96fa7838a12cdb52b61dae485
hzmarrou <zmarrouhoussam@email.com>
2026-05-18 16:57:05 +0100

    store the string inside a file using writeFile

---
+++ verydisco-forever.mjs
@@ -0,0 +1,8 @@
+import { writeFile } from "node:fs/promises"
+const arg = process.argv[2]
+const res = arg.split(" ").map(word => {
+  const first = word.slice(0, Math.ceil(word.length/2))
+  const second = word.slice(first.length , word.length)
+  return second + first
+}).join(" ")
+await writeFile("verydisco-foreever.txt", res)
