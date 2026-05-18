940bfc49e287146a59094c6ab3d74aac7a74d2ad
hzmarrou <zmarrouhoussam@email.com>
2026-05-18 18:34:54 +0100

    FIX SORT

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -1,12 +1,13 @@
 import { readdir } from "node:fs/promises"
 const dir = process.argv[2]
 const files = await readdir(dir)
-files.sort()
+files.sort((a, b) => {
+  const lastA = a.split(".")[0].split("_")[1]
+  const lastB = b.split(".")[0].split("_")[1]
+  return lastA.localeCompare(lastB)
+})
 const result = files.map((file, index) => {
   const parts = file.split(".")
   const [firstName, lastName] = parts[0].split("_")
-  const first = firstName.slice(0, 1).toUpperCase() + firstName.slice(1)
-  const last = lastName.slice(0, 1).toUpperCase() + lastName.slice(1)
-  return `${index + 1}. ${last} ${first}`
+  return `${index + 1}. ${lastName} ${firstName}`
 })
-console.log(result.join("\n"))
