4aae2d9966ef1cf766520fc44413765c871b360a
hzmarrou <zmarrouhoussam@email.com>
2026-05-18 18:32:03 +0100

    small fix

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -4,7 +4,7 @@ const files = await readdir(dir)
 files.sort()
 const result = files.map((file, index) => {
   const parts = file.split(".")
-  const [firstName, lastName] = parts[0].split("-")
+  const [firstName, lastName] = parts[0].split("_")
   const first = firstName.slice(0, 1).toUpperCase() + firstName.slice(1)
   const last = lastName.slice(0, 1).toUpperCase() + lastName.slice(1)
   return `${index + 1}. ${last} ${first}`
