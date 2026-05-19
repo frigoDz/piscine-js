59f29b63cfa61b9804d57b83477524d33b53c32a
hzmarrou <zmarrouhoussam@email.com>
2026-05-19 15:49:40 +0100

    finnaly corrected syntax

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -1,13 +1,9 @@
 import { readdir } from "node:fs/promises"
-const dir = process.argv[2]
+const dir = process.argv[2] || "."
 const files = await readdir(dir)
-files.sort((a, b) => {
-  const lastA = a.split(".")[0].split("_")[1]
-  const lastB = b.split(".")[0].split("_")[1]
-  return lastA.localeCompare(lastB)
+let formated = files.map((file) => {
+  let [name] = file.split(".")
+  return name.split("_")[1]+" " + name.split("_")[0]
 })
-const result = files.map((file, index) => {
-  const parts = file.split(".")
-  const [firstName, lastName] = parts[0].split("_")
-  return `${index + 1}. ${lastName} ${firstName}`
-})
+let res = formated.sort()
+res.forEach((name,index) => console.log(index + 1 + " " +name))
