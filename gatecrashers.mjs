2d037375789da311eb7d72586207b33cbe3c7e1a
hzmarrou <hzmarrou@gmail.com>
2026-05-19 18:44:09 +0100

    fix

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -1,23 +1,29 @@
 import { createServer } from "node:http"
 import { writeFile } from "node:fs/promises"
 import { join } from "node:path"
+
 const allowed = ["Caleb_Squires", "Tyrique_Dalton", "Rahima_Young"]
 const password = "abracadabra"
+
 const server = createServer(async (req, res) => {
   res.setHeader("Content-Type", "application/json")
+
   const auth = req.headers["authorization"]
   if (!auth) {
     res.writeHead(401)
-    res.end(JSON.stringify({ error: "Authorization Required" }))
+    res.end("Authorization Required")
     return
   }
+
   const base64 = auth.split(" ")[1]
   const [user, pass] = Buffer.from(base64, "base64").toString().split(":")
+
   if (!allowed.includes(user) || pass !== password) {
     res.writeHead(401)
-    res.end(JSON.stringify({ error: "Authorization Required" }))
+    res.end("Authorization Required")
     return
   }
+
   if (req.method === "POST") {
     let body = ""
     req.on("data", (chunk) => {
@@ -28,8 +34,8 @@ const server = createServer(async (req, res) => {
         const name = req.url.slice(1)
         const fpath = join(process.cwd(), "guests", `${name}.json`)
         await writeFile(fpath, body)
-        res.writeHead(201)
-        res.end(body)
+        res.writeHead(200)
+        res.end(JSON.stringify(JSON.parse(body)))
       } catch (err) {
         res.writeHead(500)
         res.end(JSON.stringify({ error: "server failed" }))
@@ -37,4 +43,5 @@ const server = createServer(async (req, res) => {
     })
   }
 })
+
 server.listen(5000, () => console.log("Listening on port 5000"))
