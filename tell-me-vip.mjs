d06f0593fedb1cc388d7057aa5ad0d0edc623ff4
hzmarrou <hzmarrou@gmail.com>
2026-05-19 17:13:07 +0100

    tell-me-vip.mjs

---
+++ tell-me-vip.mjs
@@ -0,0 +1,19 @@
+import { readFile, writeFile } from "node:fs/promises"
+import { join } from "node:path"
+const dir = process.argv[2]
+const files = await readdir(dir)
+let guests = []
+for (const file of files) {
+  const content = await readFile(join(dir, file), "utf-8")
+  const data = JSON.parse(content)
+  if (data.answer === "YES") {
+    let [name] = file.split(".")
+    let firstname = name.split("_")[1]
+    let lastname = name.split("_")[0]
+    guests.push(lastname + " " + firstname)
+  }
+}
+guests.sort()
+const res = guests.map((name, i) => `${i + 1}. ${name}`).join("\n")
+await writeFile("vip.txt", res)
+console.log(res)
